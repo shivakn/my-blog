bc7f6b736db666b989e8730d119526b2ffd9cf42
shiva kn <shiva.knsk@gmail.com>
2020-09-11 18:24:16 +0530

    first commit

---
+++ src/build/precache-manifest.c4938767c43ea23d90ed3fb38cb2dfdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e8d5f2b23ee2fbffe58d9350d3190a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3915850e8d1a29f1b85",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9f27527568c48733aa8c",
+    "url": "/static/js/2.ca0bc356.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca0bc356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3915850e8d1a29f1b85",
+    "url": "/static/js/main.6c977281.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
